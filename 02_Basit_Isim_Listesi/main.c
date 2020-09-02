181dcbb90677fa1ce77555dbec6ad9a8b2901a3a
kagancansit <kagancansit@hotmail.com>
2020-09-02 17:45:52 +0300

    isim_listesi_orn

--- 02_Basit_Isim_Listesi/main.c
+++ 02_Basit_Isim_Listesi/main.c
@@ -11,14 +11,22 @@ int main() {
 	//printf("13.06.2020 - 22:28 Kagan Can Sit");
 
 
-	//Basit bir liste ve görünüm için örnek bir yazým.
+	//Basit bir liste görünümlü isim listesi için örnek bir yazým.
 	
 	printf("***************Kisi Bilgi******************\n");
 	printf("Ad: Kagan Can\n");
 	printf("Soyad: Sit\n");
 	printf("Meslek: Ogrenci\n");
 	printf("Sevdigi 3 Ugras: Photoshop, Yazilim, Film\n");
+	
+	printf("Ad: Ali\n");
+	printf("Soyad: Uzun\n");
+	printf("Meslek: Doktor\n")
+	printf("Sevdigi 3 Ugras: Kitap Okumak, Bisiklet, Yuruyus");
+	
 	printf("*********************************************");
+	
+	
 	
 	return 0;
 }
