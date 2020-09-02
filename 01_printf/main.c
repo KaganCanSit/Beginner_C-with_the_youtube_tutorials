fc6065aa6281a6493b4f017fdbd6c3a99f6d0d72
kagancansit <kagancansit@hotmail.com>
2020-09-02 16:31:40 +0300

    ilkdersdeneme

---
+++ 01_printf/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	/*
+	printf(""); --->> Týrnak içerisine yazdýrmak istediðimiz metini gireriz.
+	printf: Temel ekran çýktýsý komutudur. Ekrana herhangi bir deðer veyaz yazý yazmak amacýyla kullanýlýr. Fakat Türkçe karakterleri içinde barýndýrmamasý gerekir, Ýngilizce dilini baz alarak yazýnýz.
+	
+	Dünya çapýnda yazýlýmýn ilk dersi gelenek olarak "Merhaba Dunya" Komutunun yazdýrýlmasýdýr.	
+	printf("Hello World");
+	
+	\n komutu bir alt satýra geçme komutudur. \n komutuna geldiðinde program bir alt satýrdan devam eder.
+	*/
+	
+	printf("Merhaba Dunya\n");	
+	printf("Kagan Can Sit\n");
+	printf("Kendine Bilgi, Deneyim Ve Yeni Fikirler Katmak Isteyen Biri\n");
+		
+	return 0;
+}
