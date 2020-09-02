f530ac6435690facbd97ef9cb82b3bc781744d93
kagancansit <kagancansit@hotmail.com>
2020-09-02 16:37:13 +0300

    basit_ornek

---
+++ 02_Basit_Isim_Listesi/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	
+	//Hello World
+	
+	//printf("Merhaba Dunya\n");
+	//printf("13.06.2020 - 22:28 Kagan Can Sit");
+
+
+	//Basit bir liste ve görünüm için örnek bir yazým.
+	
+	printf("***************Kisi Bilgi******************\n");
+	printf("Ad: Kagan Can\n");
+	printf("Soyad: Sit\n");
+	printf("Meslek: Ogrenci\n");
+	printf("Sevdigi 3 Ugras: Photoshop, Yazilim, Film\n");
+	printf("*********************************************");
+	
+	return 0;
+}
